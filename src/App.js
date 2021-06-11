3f4f660fbac59ebbfcfb0be6f58bc64306161700
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2021-06-11 13:49:38 -0600

    basic layout of page created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Table from './Table'
+import SidePane from './components/SidePane'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
+      <SidePane />
+      <Nav />
       <Table />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import logo from '../assets/img/deVillageLogo.png'
+import '../SCSS/Nav.scss'
+function Nav() {
+    return (
+        <nav class='nav'>
+            <img src={logo}></img>
+            <section>
+                {/* <NavLink activeClassName="selected" className='navlink' to="/quotes">
+                    View Quotes
+                </NavLink>
+                <NavLink activeClassName="selected" className='navlink' to="/new">
+                    Add Quote
+                </NavLink>
+                {/* <Tabs>
+                    <Tab id="tab-1" to="/quotes" label="View Quotes"></Tab>
+                    <Tab id="tab-2" to="/new" label="Add Quote"></Tab>
+                </Tabs> */}
+            </section> 
+        </nav>
+    )
+}
+
+export default Nav
